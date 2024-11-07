c5386c41691fc4fd890b12050683b9b39c3cb6fe
Favour <168464842+Justreadin@users.noreply.github.com>
2024-11-07 13:52:11 +0100

    Create TaskBase.java

---
+++ Java_package/Abstract_packages/TaskBase.java
@@ -0,0 +1,24 @@
+package Abstract_packages;
+
+public abstract class TaskBase {
+    protected String taskDescription;
+    protected int taskId;
+
+    public abstract void completeTask();
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+    public int getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(int taskId) {
+        this.taskId = taskId;
+    }
+}
