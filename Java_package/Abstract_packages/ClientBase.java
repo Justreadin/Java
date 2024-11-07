99ec03d8aec1a152dc4fa5fff7364c3007b46b63
Favour <168464842+Justreadin@users.noreply.github.com>
2024-11-07 13:51:09 +0100

    Create ClientBase.java

---
+++ Java_package/Abstract_packages/ClientBase.java
@@ -0,0 +1,24 @@
+package Abstract_packages;
+
+public abstract class ClientBase {
+    protected String clientName;
+    protected String clientAddress;
+
+    public abstract void handleClientRequest();
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public String getClientAddress() {
+        return clientAddress;
+    }
+
+    public void setClientAddress(String clientAddress) {
+        this.clientAddress = clientAddress;
+    }
+}
