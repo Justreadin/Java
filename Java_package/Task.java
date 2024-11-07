ac510ff11ed873496eeea8c9954eddad9160f95a
Favour <168464842+Justreadin@users.noreply.github.com>
2024-11-07 13:32:05 +0100

    Create Task.java

---
+++ Java_package/Task.java
@@ -0,0 +1,27 @@
+package Java_package;
+
+public class Task {
+    private String taskDescription;
+    private int taskId;
+
+    public Task(String taskDescription, int taskId) {
+        this.taskDescription = taskDescription;
+        this.taskId = taskId;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+    public int getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(int taskId) {
+        this.taskId = taskId;
+    }
+}
